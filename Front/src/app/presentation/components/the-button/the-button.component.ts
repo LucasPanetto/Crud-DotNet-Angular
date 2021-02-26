e8c15b4173fe24dbdc042d2c7aa55de1d903ce3c
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-02-26 00:50:16 -0300

    feat: criado componente para botão

---
+++ Front/src/app/presentation/components/the-button/the-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { EventEmitter, Output } from '@angular/core'
+
+@Component({
+  selector: 'app-the-button',
+  templateUrl: './the-button.component.html',
+  styleUrls: ['./the-button.component.sass']
+})
+export class TheButtonComponent implements OnInit {
+  @Input() text: string = 'Botão'
+  @Input() propertieClass: any = ''
+
+  @Output() buttonClick = new EventEmitter()
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
