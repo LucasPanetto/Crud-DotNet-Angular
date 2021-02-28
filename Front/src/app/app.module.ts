4b8b9b13f4651d52a6b801647c39e2a83fb629d4
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-02-28 18:35:38 -0300

    feat: importando componentes da barra lateral

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TheMenuComponent } from './presentation/components/the-sidebar/the-menu/the-menu.component';
+import { TheSidebarComponent } from './presentation/components/the-sidebar/the-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheSidebarComponent,
+    TheMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
