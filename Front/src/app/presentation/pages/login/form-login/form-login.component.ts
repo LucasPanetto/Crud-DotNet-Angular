cd71b49629d8a71a918d26c481c9b31dbd7107d3
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-02-28 18:38:32 -0300

    feat: implementando validação de formulário no login

--- Front/src/app/presentation/pages/login/form-login/form-login.component.ts
+++ Front/src/app/presentation/pages/login/form-login/form-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-login.component.sass']
 })
 export class FormLoginComponent implements OnInit {
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required]], //wsilvas
+      password: ['Kg#84bbbcb8', Validators.required], //Kg#84bbbcb8
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  tryLogin() {
+    if (this.loginForm.invalid) {
+      console.log('is invalid')
+      this.loginForm.markAllAsTouched()
+      return
+    }
+
+    console.log('Indo pra HOME')
+    this.router.navigate(['home']);
+
   }
 
 }
--- Front/src/app/presentation/pages/login/login.module.ts
+++ Front/src/app/presentation/pages/login/login.module.ts
@@ -5,9 +5,10 @@ import { LoginRoutingModule } from './login-routing.module'
 import { LoginComponent } from './login.component'
 import { FormLoginComponent } from './form-login/form-login.component'
 import { AsideLoginComponent } from './aside-login/aside-login.component'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TheInputModule } from '../../components/the-input/the-input.module'
 import { TheButtonModule } from '../../components/the-button/the-button.module';
+import { ValidateMessageModule } from '../../components/validate-message/validate-message.module'
 
 @NgModule({
   declarations: [LoginComponent, FormLoginComponent, AsideLoginComponent],
@@ -16,7 +17,7 @@ import { TheButtonModule } from '../../components/the-button/the-button.module';
     LoginRoutingModule,
     FormsModule,
     TheInputModule,
-    TheButtonModule
+    TheButtonModule, ReactiveFormsModule, ValidateMessageModule
   ],
 })
 
