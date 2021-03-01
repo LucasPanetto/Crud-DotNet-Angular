0e1128e31ad518a41449515ed907f668105e031c
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-02-28 23:42:32 -0300

    feat: importando pacotes responsaveis pelo consumo da api

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TheMenuComponent } from './presentation/components/the-sidebar/the-menu/the-menu.component';
 import { TheSidebarComponent } from './presentation/components/the-sidebar/the-sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RequestService } from './core/services/request.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     TheSidebarComponent,
-    TheMenuComponent
+    TheMenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
