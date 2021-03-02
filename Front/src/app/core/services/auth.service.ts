2a96fe1ea0ebd7ab68bfd3c5c088380bf8e1517f
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-03-02 19:53:52 -0300

    feat: finalizado serviço responsavel por autenticação

--- Front/src/app/core/services/auth.service.ts
+++ Front/src/app/core/services/auth.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-/* import { ServerService } from './server.service'
- */import { catchError } from 'rxjs/operators'
 import { BehaviorSubject } from 'rxjs'
-/* import * as jwt_decode from 'jwt-decode'
- */import { UserModel } from '../domain/user.model'
+import { UserModel } from '../domain/user.model'
+import { LoginResultModel } from '../domain/loginResult.model'
+import { RequestService } from './request.service'
 
 @Injectable({
   providedIn: 'root',
@@ -12,24 +10,8 @@ import { BehaviorSubject } from 'rxjs'
 export class AuthService {
   loggedIn = new BehaviorSubject<boolean>(false)
 
-  /*  constructor(private http: HttpClient, private server: ServerService) {
-     this.loggedIn.next(this.getToken() ? true : false)
-   } */
-
-  /* loginUser(params: UserModel) {
-    return this.http
-      .post<any>(this.server.url(`/Login`), params)
-      .pipe(catchError(this.server.handleError))
-  }
-
-  forgotPassword(userName: string) {
-    return this.http
-      .post<any>(this.server.url(`/Login/senha/resetar`), userName)
-      .pipe(catchError(this.server.handleError))
-  }
-
-  getToken(): string {
-    return localStorage.getItem('access-token')
+  constructor(private requestService: RequestService) {
+    this.loggedIn.next(this.isValidLogged() ? true : false)
   }
 
   logout(): void {
@@ -37,32 +19,19 @@ export class AuthService {
     window.location.reload()
   }
 
-  decode() {
-    let token = { sid: '', id: '' }
-    if (this.getToken()) {
-      token = jwt_decode(this.getToken())
-      return token
+  isValidLogged(): boolean {
+    if (localStorage.getItem('user')) {
+      const storageUser: UserModel = JSON.parse(localStorage.getItem('user') || '{}');
+      const isValid = this.requestService.login(storageUser?.userName, storageUser.password).subscribe((data: LoginResultModel) => { return data.success })
+      return isValid ? true : false
     }
-    return token
+    return false
   }
-
-  refreshToken() {
-    const { id } = this.decode()
-    const refreshToken = localStorage.getItem('refresh-token')
-
-    return this.http
-      .post<any>(this.server.url('/Login'), {
-        userName: id,
-        password: refreshToken,
-        grantTypes: 'refresh_token',
-        resultado: 0,
-      })
-      .pipe(catchError(this.server.handleError))
-  }*/
 
   get isLoggedIn() {
     return this.loggedIn.asObservable()
   }
+
 
   /*  getDataUser(): DataUser {
      return JSON.parse(localStorage.getItem('user'))
