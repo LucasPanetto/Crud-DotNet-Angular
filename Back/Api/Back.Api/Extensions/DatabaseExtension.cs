b1df4f8dc357d943aaa116fcf746b108937dba0c
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-03-01 23:12:03 -0300

    feat: implementado novo projeto backend

---
+++ Back/Api/Back.Api/Extensions/DatabaseExtension.cs
@@ -0,0 +1,31 @@
+﻿using Back.Infrastructure.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Back.Api.Extensions
+{
+    public static class DatabaseExtension
+    {
+        public static void AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+        {
+
+            if (environment?.EnvironmentName == "Testing")
+            {
+                services.AddDbContextPool<ProductDbContext>(o =>
+                {
+                    o.UseSqlite("Data Source=test.db");
+                });
+            }
+            else
+            {
+                services.AddDbContextPool<ProductDbContext>(o =>
+                {
+                    o.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                });
+            }
+
+        }
+    }
+}
---
+++ Back/Api/Back.Application/DTOs/Product/GetProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Back.Application.DTOs.Product
+{
+    public class GetProductDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal price { get; set; }
+        public string ImageBase64 { get; set; }
+    }
+}
