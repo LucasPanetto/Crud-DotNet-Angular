f202d14ddc59321725cae9f361d80694e4f60105
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-03-02 00:16:42 -0300

    refactor: alterado padrão de diretório

---
+++ Back/Api/src/Back.Api/Startup.cs
@@ -0,0 +1,85 @@
+using Back.Api.Extensions;
+using Back.Application.Interfaces;
+using Back.Application.Services;
+using Back.Domain.Repositories;
+using Back.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Back.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationDbContext(Configuration, Environment);
+
+            //DI Services and Repos
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductAppService, ProductAppService>();
+
+            // WebApi Configuration
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); // for enum as strings
+            });
+
+            // AutoMapper settings
+            services.AddAutoMapperSetup();
+
+            // HttpContext for log enrichment 
+            services.AddHttpContextAccessor();
+
+            // Swagger settings
+            services.AddApiDoc();
+
+            // GZip compression
+            services.AddCompression();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseApiDoc();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Back/Api/src/Back.Application/Filters/GetProductsFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Back.Application.Filters
+{
+    public class GetProductsFilter
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageBase64{ get; set; }
+
+    }
+}
