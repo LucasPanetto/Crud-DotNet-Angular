5cd4728337765bbad0553b9e6d6eae02aa1c9941
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-03-03 01:00:44 -0300

    feat: implementando rota de produtos e authguard

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { AuthGuard } from './core/guards/auth.guard'
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -10,8 +11,15 @@ const routes: Routes = [
   },
   {
     path: 'home',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./presentation/pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'products',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./presentation/pages/products/products.module').then((m) => m.ProductsModule),
   },
 ]
 
