432524ca76400c589019efd5a8fd3f7df84dc164
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-03-04 19:54:34 -0300

    feat: adicionando scripts para gerar database

---
+++ Database/SCPT_Tabela.sql
@@ -0,0 +1,23 @@
+USE [CRUDProduct]
+GO
+
+/****** Object:  Table [dbo].[Products]    Script Date: 04/03/2021 13:56:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[Price] [float] NOT NULL,
+	[ImageBase64] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
